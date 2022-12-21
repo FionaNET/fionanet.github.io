19cc42fb90ff91a6e6370f94c94dcbe799418b9c
Fiona Lin <xwhitewhalex@live.com>
2022-12-21 22:39:26 +1300

    Add color picker and change from sketch to etch

--- Etch-a-sketch/etch.js
+++ Etch-a-sketch/etch.js
@@ -24,13 +24,26 @@ function createEtchGrid() {
 
     // Add event handlers for all of the etch squares
     const etch_squares = document.querySelectorAll('.flex_vertical_item');
+    // To draw
+    // etch_squares.forEach((etch_square) => {
+    //   // and for each one we add a 'click' listener
+    //   etch_square.addEventListener('click', () => {
+    //     if (etch_square.classList.contains("flex_vertical_item_pressed")) {
+    //       etch_square.classList.remove("flex_vertical_item_pressed");
+    //     } else {
+    //       etch_square.classList.add("flex_vertical_item_pressed");
+    //     }
+
+    //   });
+    // });
+
+    // To Etch
     etch_squares.forEach((etch_square) => {
       // and for each one we add a 'click' listener
-      etch_square.addEventListener('click', () => {
-        if (etch_square.classList.contains("flex_vertical_item_pressed")) {
-          etch_square.classList.remove("flex_vertical_item_pressed");
-        } else {
+      etch_square.addEventListener('mouseover', () => {
+        if (!etch_square.classList.contains("flex_vertical_item_pressed")) {
           etch_square.classList.add("flex_vertical_item_pressed");
+          etch_square.classList.remove("flex_vertical_item");
         }
 
       });
@@ -60,7 +73,27 @@ function openWarning() {
   });
 }
 
+function changeEtchColor() {
+  const color = document.getElementById("etch_color").value;
+  var header = getCSSRule('div.flex_vertical_item_pressed');
+  header.style.backgroundColor = color;
+}
 //Handlers
 document.getElementById("submit_grid_size")?.addEventListener('click', createEtchGrid);
 document.getElementById("error_ok").addEventListener('click', closeWarning);
+document.getElementById("etch_color").addEventListener('blur', changeEtchColor);
 
+function getCSSRule(selector) {
+  selector = selector.toLowerCase();
+  var result = null;
+  var find = Array.prototype.find;
+
+  find.call(document.styleSheets, styleSheet => {
+    result = find.call(styleSheet.cssRules, cssRule => {
+      return cssRule instanceof CSSStyleRule
+        && cssRule.selectorText.toLowerCase() == selector;
+    });
+    return result != null;
+  });
+  return result;
+}
